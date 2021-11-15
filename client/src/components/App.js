ca4f88a38b0397743ca6a94e54232db314f3b274
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-15 19:35:46 +0530

    Got the data using axios and done some html structure.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "../styles/App.css"
+import Body from './Body'
 import Header from './Header'
 
 const App = () => {
   return (
     <div>
       <Header/>
+      <Body/>
     </div>
   )
 }
---
+++ client/src/components/Body.js
@@ -0,0 +1,26 @@
+import React, {useEffect} from 'react'
+import axios from 'axios'
+import "../styles/Body.css"
+
+const Body = () => {
+
+    useEffect(() => {
+        axios.get('http://localhost:3004/data')
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }, [])
+
+    return (
+        <div style={{padding:"0 60px"}}>
+            <div style={{padding:"30px 40px"}}>
+                Body
+            </div>
+        </div>
+    )
+}
+
+export default Body
