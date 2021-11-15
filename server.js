e8fd0791a300ac4de48d3d02e7663d05c625a514
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-15 19:23:56 +0530

    Implemented axios and got all the data.

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 const port = 3004;
 
 app.get('/', (req, res) => {
     res.json("Root folder.");
+})
+
+app.get('/data',(req,res) => {
+    axios.get('https://fakestoreapi.com/products')
+        .then((response) => {
+            res.json(response.data)
+        })
+        .catch((err) => {
+            res.json(err);
+        })
 })
 
 app.listen(port, () => {
