3041176cf1e23f98af9b8811474a9a302a03dffa
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-16 14:11:16 +0530

    Added single product.

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -16,7 +16,21 @@ const Body = () => {
 
     return (
         <div style={{padding:"30px 40px"}}>
-            Body
+            <h1 style={{textAlign:"center"}}>Products</h1>
+                <div style={{paddingTop:"20px"}}>
+                    <div style={{backgroundColor:"#bfbfbf", borderRadius:"5px", padding:"20px", width:"20%"}}>
+                        <h3>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h3>
+                        <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Product1" width="100%" style={{paddingTop:"10px"}}/>
+                        <div style={{display: 'flex', justifyContent: 'space-between', paddingTop:"10px"}}>
+                            <h4><i>Price:</i> 109.95</h4>
+                            <h4><i>Rating:</i> 3.9(120)</h4>
+                        </div>
+                        <div style={{display: 'flex', justifyContent: 'center', paddingTop:"10px"}}>
+                            <button className="cartButton"style={{height: '40px'}}>Add to Cart</button>
+                        </div>
+                        <h4 style={{paddingTop:"10px"}}>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</h4>
+                    </div>
+                </div>
         </div>
     )
 }
